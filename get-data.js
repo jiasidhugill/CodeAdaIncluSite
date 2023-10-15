044bab3128f4b759a51768ae08f075e878db8a21
Jia Gill <96542484+jiasidhugill@users.noreply.github.com>
2023-10-15 16:07:25 -0500

    Create get-data.js

---
+++ get-data.js
@@ -0,0 +1,58 @@
+// sets up MongoDB
+const {MongoClient} = require("mongodb");
+
+// gets data from API
+async function getAPI(url) {
+    var response = await fetch(url);
+   
+    // Storing data in form of JSON
+    var data = await response.json();
+    console.log(data);
+
+    return data;
+}
+
+async function addData(client, newData) {
+    const result = await client.db("WAVEFedWebsiteData").collection("FedWebsDataRT2").insertOne(newData);
+    console.log(`New data added with ID ${result.insertedId}`)
+}
+
+// putting data into MongoDB database
+async function main() {
+    const uri = "mongodb+srv://<username>:<password>@wavefedwebsitedata.tnc4kdt.mongodb.net/?retryWrites=true&w=majority";
+    const client = new MongoClient(uri);
+
+    //formatting data for websites
+    var webData = await import("./all-domains-30-days.json", {
+        assert: { type: "json" },
+    });
+    webData = webData.default;
+
+    var lastIndex = 0;
+    var credNum = 50;
+    var fedURL = "";
+    var APIKey = ""; // add API key here 
+
+    try {
+        await client.connect();
+
+        for (var i = lastIndex; i < lastIndex + credNum; i++) {
+            fedURL = webData[i].domain;
+            url = `https://wave.webaim.org/api/request?key=${APIKey}&url=${fedURL}&reporttype=2`;
+            console.log(url);
+
+            var newData = await getAPI(url);
+            await addData(client, newData);
+        }
+
+        // // use to double-check index: 
+        // console.log(webData[lastIndex - 1].domain)
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main();
+  
